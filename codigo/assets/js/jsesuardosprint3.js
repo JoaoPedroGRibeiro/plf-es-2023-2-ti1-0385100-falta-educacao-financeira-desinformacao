97fea90e7fafb60df611d25422d554149386b782
Eduardo <eduardo.henriquems@gmail.com>
2023-11-29 10:37:13 -0300

    Quiz e calculadoras ligadas

--- codigo/assets/js/jsesuardosprint3.js
+++ codigo/assets/js/jsesuardosprint3.js
@@ -1,24 +1,19 @@
-window.addEventListener('DOMContentLoaded', () => {
-    fetch('https://jsonserver--eduardomoreir20.repl.co/contatos')
-        .then((response) => response.json())
-        .then((dataJSON) => {
-            let info = ''; 
-            for (let i = 0; i < dataJSON.length; i++) {
-                info += `
-                    <div id="usuario">
-                        <h2>Id Usuário: ${dataJSON[i].id}</h2>
-                    </div>
-                    <div id="pontos">
-                        <h3>Pontuação: ${dataJSON[i].valortotal}</h3>
-                    </div>
-                    <div id="perfil">
-                        <h4>Perfil de investidor: ${dataJSON[i].resultado}</h4>
-                    </div>`;
-            }
-            
-           document.getElementById('infos').innerHTML= info;
-        });
+window.addEventListener("DOMContentLoaded", () => {
+  fetch("https://jsonservertiaw--robertasophiacs.repl.co/perfil?id=1")
+    .then((response) => response.json())
+    .then((dataJSON) => {
+      const dados = dataJSON[0];
+      let info = `
+            <div id="usuario">
+                <p><b>Id Usuário:</b> ${dados.id}</p>
+            </div>
+            <div id="pontos">
+                <p><b>Pontuação:</b> ${dados.valorTotal}</p>
+            </div>
+            <div id="perfil">
+                <p><b>Perfil de investidor:</b> ${dados.resultado}</p>
+            </div>`;
+
+      document.getElementById("infos").innerHTML = info;
+    });
 });
-
-
-
